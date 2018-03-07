23226066df33b75dcaaf685d3ec6df74a4586015
andokazu1979 <ando.kazuto@gmail.com>
2018-03-07 18:40:31 +0900

    Fix corresponding to shell parameter

--- go.sh
+++ go.sh
@@ -10,16 +10,16 @@
 #  py: Number of process for y-axis
 #  pz: Number of process for z-axis
 
-nx=$1
-ny=$2
-nz=$3
-nt=$4
-px=$5
-py=$6
-pz=$7
-pt=$8
-input=$9
-output=$10
+nx=${1}
+ny=${2}
+nz=${3}
+nt=${4}
+px=${5}
+py=${6}
+pz=${7}
+pt=${8}
+input=${9}
+output=${10}
 np=$(expr $px \* $py \* $pz \* $pt)
 
 mpirun -n ${np} python ./main.py ${nx} ${ny} ${nz} ${nt} ${px} ${py} ${pz} ${pt} ${input} ${output}
