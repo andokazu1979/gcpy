6cb9b719e75502806e962996d188c3ef32dfefb1
andokazu1979 <ando.kazuto@gmail.com>
2018-03-14 10:52:56 +0900

    Rearrange the relation between library and driver

---
+++ analyzer.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+
+import sys
+import logging
+
+from mpi4py import MPI
+
+import gtimer
+
+level_ = logging.DEBUG
+# level_ = logging.INFO
+# level_ = logging.WARNING
+# level_ = logging.ERROR
+# level_ = logging.CRITCAL
+
+
+class Analyzer(object):
+
+    def __init__(self):
+        """Initialization for MPI"""
+
+        comm = MPI.COMM_WORLD
+        nproc = comm.Get_size()
+        rank = comm.Get_rank()
+        
+        logging.basicConfig(filename='log.{0:06d}'.format(rank), filemode='w', level=level_)
+        self.logger = logging.getLogger(__name__)
+        
+        gtimer.timer_sta(1, 'Initialization')
+        
+        self.logger.debug("MPI_COMM_WORLD: nproc {0}, rank {1}".format(nproc, rank))
+
+        self.nx = int(sys.argv[1])
+        self.ny = int(sys.argv[2])
+        self.nz = int(sys.argv[3])
+        self.nt = int(sys.argv[4])
+        
+        self.px = int(sys.argv[5])
+        self.py = int(sys.argv[6])
+        self.pz = int(sys.argv[7])
+        self.pt = int(sys.argv[8])
+        
+        self.comm = comm.Create_cart((self.pt, self.pz, self.py, self.px), reorder=True)
+        self.nproc = self.comm.Get_size()
+        self.rank = self.comm.Get_rank()
+        
+        gtimer.timer_end(1)
+
+    def do_analysis(self):
+        self.pre_process()
+        self.main_process()
+        self.post_process()
+
+    def pre_process(self):
+        pass
+
+    def main_process(self):
+
+        gtimer.timer_sta(0, 'Total')
+
+        gtimer.timer_sta(2, 'Input')
+        self.read_grid()
+        gtimer.timer_end(2)
+
+        gtimer.timer_sta(3, 'Grid calculation')
+        self.grid_calc()
+        gtimer.timer_end(3)
+
+        gtimer.timer_sta(4, 'Output')
+        self.write_grid()
+        gtimer.timer_end(4)
+
+        gtimer.timer_end(0)
+
+        self.show_timer_result()
+
+    def post_process(self):
+        pass
+
+    def read_grid(self):
+        pass
+
+    def grid_calc(self):
+        pass
+
+    def write_grid(self):
+        pass
+
+    def show_timer_result(self):
+
+        for item in gtimer.get_lst_elapse():
+            self.logger.info("rank{0}: {1}".format(self.rank, item))
+
---
+++ gio.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+import sys
+import logging
+import numpy as np
+from mpi4py import MPI
+
+logger = logging.getLogger(__name__)
+
+def read_grid(comm, fname, subgrid):
+    """Read grid data"""
+
+    amode = MPI.MODE_RDONLY
+    fh = MPI.File.Open(comm, fname, amode)
+    filetype = get_filetype(comm, subgrid)
+    fh.Set_view(filetype=filetype)
+    fh.Read_at_all(0, subgrid)
+    
+    fh.Close()
+    
+    subgrid.byteswap(inplace=True)
+    
+    logger.debug("input:\n{0}\n".format(subgrid))
+
+def grid_calc(self):
+    """Grid calculation"""
+
+    pass
+            
+def write_grid(comm, fname, subgrid):
+    """Write grid data"""
+
+    logger.debug("result:\n{0}\n".format(subgrid))
+    
+    subgrid.byteswap(inplace=True)
+    
+    amode = MPI.MODE_WRONLY|MPI.MODE_CREATE
+    fh = MPI.File.Open(comm, fname, amode)
+    filetype = get_filetype(comm, subgrid)
+    fh.Set_view(filetype=filetype)
+    fh.Write_at_all(0, subgrid)
+    
+    fh.Close()
+
+def get_filetype(comm, subgrid):
+    nx = subgrid.shape[3]
+    ny = subgrid.shape[2] 
+    nz = subgrid.shape[1] 
+    nt = subgrid.shape[0] 
+
+    shape = comm.Get_topo()[0]
+    px = shape[3]
+    py = shape[2]
+    pz = shape[1]
+    pt = shape[0]
+
+    rank = comm.Get_rank()
+ 
+    subsizes = (int(nt/pt), int(nz/pz), int(ny/py), int(nx/px))
+    coords = comm.Get_coords(rank)
+    starts = (subsizes[0] * coords[0], subsizes[1] * coords[1], subsizes[2] * coords[2], subsizes[3] * coords[3])
+    filetype = MPI.FLOAT.Create_subarray(subgrid.shape, subsizes, starts, MPI.ORDER_C)
+    filetype.Commit()
+
+    return filetype
